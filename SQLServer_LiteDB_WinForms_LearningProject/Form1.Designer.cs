e69190708c3b0624aeae7c2da18b419a815322e0
Arkadiusz Zając <arekzaj7@gmail.com>
2023-05-11 00:15:50 +0200

    Add project files.

---
+++ SQLServer_LiteDB_WinForms_LearningProject/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SQLServer_LiteDB_WinForms_LearningProject
+{
+    partial class mainApplication
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mainApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1257, 690);
+            this.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "mainApplication";
+            this.Text = "Kino XYZ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SQLServer_LiteDB_WinForms_LearningProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace SQLServer_LiteDB_WinForms_LearningProject
+{
+    public partial class mainApplication : Form
+    {
+        public mainApplication()
+        {
+            InitializeComponent();
+        }
+    }
+}
